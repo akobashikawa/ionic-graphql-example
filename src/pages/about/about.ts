16e56bceb41108f83d45a0358fad141fa5b00656
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-02-12 00:19:27 -0500

    Fix import for OnInit

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { NavController } from 'ionic-angular';
 
 import { Apollo } from "apollo-angular";
