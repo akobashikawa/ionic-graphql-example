fa9633e529fd9dcd4479cd8eb0e779718fb15929
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-02-12 00:16:19 -0500

    Implement Hello to show pass parameters pattern

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-@Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
-})
-export class AboutPage {
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
 
-  constructor(public navCtrl: NavController) {
-
+const query = gql`
+  query Hello($name: String) {
+    hello(name: $name)
   }
+`;
 
+@Component({
+  selector: "page-about",
+  templateUrl: "about.html"
+})
+export class AboutPage implements OnInit {
+  loading: boolean;
+  data: {};
+
+  constructor(public navCtrl: NavController, private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery<any>({
+        query: query,
+        variables: {
+          name: "Rulo"
+        }
+      })
+      .valueChanges.subscribe(({ data }) => {
+        this.loading = data.loading;
+        console.log(data);
+        this.data = data;
+      });
+  }
 }
