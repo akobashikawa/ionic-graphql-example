b3a0cf6baf5dce7559e2cf96b01f02ed9d889bc9
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-02-12 00:04:28 -0500

    Implement basic query pattern
    As in https://www.apollographql.com/docs/angular/basics/queries.html

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { Apollo } from "apollo-angular";
 import gql from "graphql-tag";
 
+const query = gql`
+  query {
+    helloworld
+  }
+`;
+
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
-export class HomePage {
-  data = {};
-  query = gql`
-    query {
-      helloworld
-    }
-  `;
+export class HomePage implements OnInit {
+  loading: boolean;
+  data: {};
 
-  constructor(public navCtrl: NavController, apollo: Apollo) {
-    apollo.query({ query: this.query }).subscribe(res => {
-      this.data = res.data;
-      console.log(this.data);
-    });
+  constructor(public navCtrl: NavController, private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery<any>({
+        query: query
+      })
+      .valueChanges.subscribe(({ data }) => {
+        this.loading = data.loading;
+        console.log(data);
+        this.data = data;
+      });
   }
 }
